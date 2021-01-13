9933b923a00a34c42e3e4823ce16429e3ba1d3bc
Meiger00 <44222667+Meiger00@users.noreply.github.com>
2021-01-13 14:40:46 -0500

    Added automatic detection of flashcard volume

--- Flashcard Randomizer.py
+++ Flashcard Randomizer.py
@@ -2,11 +2,11 @@
 import pandas as pd
 import random
 
-# Read in the excel fie containing the flashcards
-df = pd.read_excel('./data/GRE Vocab List.xlsx', head = True)
+# Read in the excel file containing the flashcards
+df = pd.read_excel('./data/GRE Vocab List.xlsm', sheet_name = 'Master List')
 
 # Pick a random flashcard out of all available flashcards in the deck
-random = random.randint(0,5)
+random = random.randint(0,df.shape[0])
 
 # Tell the user the card the randomizer drew
 print("YOUR WORD IS: " + df.iloc[random,0])
@@ -17,19 +17,20 @@ booly = "True"
 
 # Create a while loop that will ask the user for input and check for appropriate responses
 while booly != "False":
-   
-    # Ask for the user's input regarding if they want to see the answer 
+    
+    # Ask for the user's input regarding if they want to see the answer
     definition = input("""Type 'yes' or 'y' when you want to see the answer.
                    
 Your response: """)
     print("_____________________________________________________________________________________________________________ \n")
-    
+ 
     # If the user responds with an appropriate response, print both 'sides' of the flashcard
     if definition.lower() == 'yes' or definition.lower() == 'y':
-        print("YOUR WORD WAS:     " + df.iloc[random,0])
+        print("YOUR WORD WAS:     " + df.iloc[random, 0])
         print("THE DEFINITION IS: " + df.iloc[random, 1])
         booly = "False"
     
     # If the user does NOT respond with an appropriate response, prompt them to try inputting another response
     else:
         print("Your response is invalid. Try again. \n")
+        print("_____________________________________________________________________________________________________________ \n")
